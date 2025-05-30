2bd256ca64ea815b30b355c07a8ae795b390c07e
tgncosist2 <tgncosist2@gmail.com>
2025-05-30 12:41:50 +0900

    3th commit

--- src/main/java/day0530/HelloMyBatis.java
+++ src/main/java/day0530/HelloMyBatis.java
@@ -3,6 +3,7 @@ package day0530;
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
@@ -13,18 +14,8 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 public class HelloMyBatis {
 	
 	public void useMyBatis() {
-		File file = new File("C:/dev/workspace/mybatis_prj/src/main/java/day0530/mybatis-config.xml");
-		if (file.exists()) {
-			try {
-				// 1. 설정용 XML과 연결해야됭
-				Reader reader = Resources.getResourceAsReader("day0530/mybatis-config.xml");
-				
-				// 2. MyBatis Framework 생성
-				SqlSessionFactory ssf = new SqlSessionFactoryBuilder().build(reader);
-				
-				// 3. MyBatis Handler를 얻어주세요
-				SqlSession ss = ssf.openSession();
-				
+				SqlSession ss = MyBatisSingleton.getSession();
+
 				// 4. Handler를 사용한 조작
 				int cnt = ss.selectOne("kr.co.sist.hello.webMemberCnt");
 				System.out.println(cnt + "건");
@@ -35,10 +26,6 @@ public class HelloMyBatis {
 				for (Dept item : dept) {
 					System.out.println(item.getDeptno() + "/" + item.getDname() + "/" + item.getLoc());
 				}// end for
-			} catch (IOException e) {
-				e.printStackTrace();
-			}// end try-catch
-		}// end if
 		
 	}// useMyBatis
 	
